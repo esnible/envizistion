9a6465eb448547bc6c314e184ad49cd8ad550845
Ed Snible <snible@us.ibm.com>
2019-02-05 13:15:03 -0500

    Apache 2 license

--- genhtml.js
+++ genhtml.js
@@ -1,9 +1,17 @@
 // Licensed Materials - Property of IBM
 // (C) Copyright IBM Corp. 2018. All Rights Reserved.
-// US Government Users Restricted Rights - Use, duplication or
-// disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-// Copyright 2018 IBM Corporation
 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 "use strict";
 
@@ -390,7 +398,9 @@ function htmlListener(listener, stats, certs) {
 		if (listenerStats.downstream_rq_2xx > 0) {
 			console.log("  Successful HTTP 2xx " + listenerStats.downstream_rq_2xx + "<br>");
 		} else if (listener.name != "virtual") {
-			console.log("  <span class='warning'>Warning no successful HTTP</span><br>");
+			if (!authnShown) {
+				console.log("  <span class='warning'>Warning no successful HTTP</span><br>");
+			}
 		}
 		if (listenerStats.downstream_rq_4xx > 0
 				|| listenerStats.downstream_rq_5xx > 0) {
@@ -541,7 +551,7 @@ function htmlCluster(cluster, stats, certs) {
 				console.log("  => " + host.socket_address.address + ":" + host.socket_address.port_value + "<br>");
 		    }
 		}
-	} else {
+	} else if (cluster.type) {
 		console.log("  => " + cluster.type + "<br>");
 	}
 
@@ -565,8 +575,9 @@ function htmlCluster(cluster, stats, certs) {
 	}
 	if (stats.cluster[cluster.name].upstream_rq_2xx > 0) {
 		console.log("  Successful HTTP 2xx " + stats.cluster[cluster.name].upstream_rq_2xx + "<br>");
-	} else if (cluster.name.startsWith('inbound|')) {
-		console.log("<span class='warning'>WARNING No successful HTTP traffic</span><br>");
+// Comment out because we don't expect these if cluster stats turned off
+//	} else if (cluster.name.startsWith('inbound|')) {
+//		console.log("<span class='warning'>WARNING No successful HTTP traffic</span><br>");
 	}
 	if (stats.cluster[cluster.name].upstream_rq_3xx > 0) {
 		console.log("REDIRECTS " + renderBreakdown(stats.cluster[cluster.name], /upstream_rq_(3[0-9][0-9])/) + "<br>");
